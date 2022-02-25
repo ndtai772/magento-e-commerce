345c591c770724e43929bc3ab0b36872628382ef
ND Tai <ndtai772@gmail.com>
2022-02-25 23:05:33 +0700

    check if credentials exits

--- scripts/make_credentials.sh
+++ scripts/make_credentials.sh
@@ -1,4 +1,9 @@
 # !/bin/bash
+if test -f "credentials/auth.json"; then
+    echo "credentials exists"
+    exit 0
+fi
+
 if [[ -z "${PUBLIC_KEY}" ]]; then
     read -p "public-key  :" PUBLIC_KEY
 fi
@@ -7,13 +12,11 @@ if [[ -z "${PRIVATE_KEY}" ]]; then
     read -p "private-key :" PRIVATE_KEY
 fi
 
-echo "
-{
+echo "{
     \"http-basic\": {
         \"repo.magento.com\": {
             \"username\": \"$PUBLIC_KEY\",
             \"password\": \"$PRIVATE_KEY\"
         }
     }
-}
-"
+}"
