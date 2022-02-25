b6ef5cc09944d7455d4c5d077d29b1fbba46bd5e
ND Tai <ndtai772@gmail.com>
2022-02-25 11:37:16 +0700

    remove debug log

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-echo "ls -la ."
-ls -la .
-
-echo "ls -la /var/www/.composer"
-ls -la /var/www/.composer
-
-echo "ls -la /var/www/.coposer/cache"
-ls -la /var/www/.composer/cache
-
 composer update
 
 bin/magento setup:install \
