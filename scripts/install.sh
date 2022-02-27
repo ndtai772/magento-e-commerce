7d969b588ca4ac608a9fd7d7b2514a7dc4b21c04
ND Tai <ndtai772@gmail.com>
2022-02-27 17:49:42 +0700

    fix port config

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ chmod +x /scripts/wait-for-it.sh
 echo $ELASTICSEARCH_HOST
 
 bash /scripts/wait-for-it.sh $MYSQL_HOST:3306 -t 0
+bash /scripts/wait-for-it.sh $ELASTICSEARCH_HOST:9200 -t 0
 
 bin/magento setup:install \
     --db-host=$MYSQL_HOST \
