79f4ae82e776e4ad8c57887ac5bc470c97fa6748
ND Tai <ndtai772@gmail.com>
2022-02-27 16:57:53 +0700

    seprate ci config

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -7,22 +7,23 @@ rm -f magento_ecommerce/app/etc/env.php
 rm -f magento_ecommerce/app/etc/config.php
 
 if [ "$CI" == "true" ]; then
-    MYSQL_USER="magento"
-    MYSQL_PASSWORD="magento"
-    MYSQL_DATABASE="magento_db"
-    MYSQL_HOST="127.0.0.1"
-else
-    read -p "mysql username: " MYSQL_USER
-    read -p "mysql password: " MYSQL_PASSWORD
-    read -p "mysql database: " MYSQL_DATABASE
-    MYSQL_HOST="db"
+    exit 0
 fi
+
+read -p "mysql username: " MYSQL_USER
+read -p "mysql password: " MYSQL_PASSWORD
+read -p "mysql database: " MYSQL_DATABASE
+MYSQL_HOST="db"
+ELASTICSEARCH_HOST="elasticsearch"
 
 echo "# MYSQL
 MYSQL_USER=$MYSQL_USER
 MYSQL_PASSWORD=$MYSQL_PASSWORD
 MYSQL_DATABASE=$MYSQL_DATABASE
 MYSQL_HOST=$MYSQL_HOST
+
+# ELASTICSEATCH
+ELASTICSEARCH_HOST=$ELASTICSEARCH_HOST
 
 # PHPFPM
 UID=$(id -u $(whoami))
